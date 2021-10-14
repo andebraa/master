f7c6ad342cdb1a4f71d33cf09dd6af0fbdc8b186
andebraa <andebraa@uio.no>
2021-10-14 17:22:00 +0200

    end of day

--- python/run/run_relax.py
+++ python/run/run_relax.py
@@ -16,11 +16,11 @@ from lammps_simulator.computer import GPU, CPU, SlurmGPU
 
 # User input
 temp = 2300
-simtime = 10000
+simtime = 100
 force = 0.001
 height = 115
 orientation = "100"
-grid = (1,1) 
+grid = (2,2) 
 #grid = False
 
 gpu = False
@@ -56,7 +56,7 @@ else:
 
 sim.copy_to_wd(datafile, lammps_dir + "SiC.vashishta")
 sim.set_input_script(lammps_dir + "in.relax", **var)
-
+"""
 if grid:
     if gpu:
         sim.run(computer=SlurmGPU(lmp_exec="lmp_python", slurm_args={'job-name': f'N{int(temp/100)}_{int(force*1000)}_{seed}_grid{grid[0]}_{grid[1]}'}, lmp_args={'-pk': 'kokkos newton on neigh full'}))
@@ -67,7 +67,8 @@ else:
     if gpu:
         sim.run(computer=SlurmGPU(lmp_exec="lmp_python", slurm_args={'job-name': f'N{int(temp/100)}_{int(force*1000)}_{seed}'}, lmp_args={'-pk': 'kokkos newton on neigh full'}))
     else:
-        sim.run(computer=CPU(num_procs=4, lmp_exec="lmp"), stdout=None)
+        sim.run(computer=CPU(num_procs=2, lmp_exec="lmp"), stdout=None)
+"""
 
-#sim.run(computer=CPU(num_procs=3, lmp_exec="lmp"), stdout=None)
+sim.run(computer=CPU(num_procs=3, lmp_exec="lmp"), stdout=None)
 #sim.run(computer=GPU(lmp_exec="lmp_python"), stdout=None)
