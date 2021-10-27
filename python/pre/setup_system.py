e54d42da344b844d75b0c13771f83dd84e2955ca
andebraa <andebraa@uio.no>
2021-10-27 17:10:16 +0200

    pre_utils__ and setup_system__ are old versions of my code. the new pre_utils and setup_system are even's code to see if it runs. it does not

--- python/pre/setup_system.py
+++ python/pre/setup_system.py
@@ -1,78 +1,31 @@
 """
 Crystal Aging Project
 
-Script for generating intial system, standard is tiny system
+Script for generating intial system
 
 This project is distributed under the GNU General Public License v3.
 For more information, see the LICENSE file in the top-level dictionary.
 """
-#TODO can this script replace or merge with grid setup0?
-from molecular_builder import write
-from pre_utils import gen_system, gen_grid_system, erratic_setup
-import argparse
-import numpy as np
 
-parser = argparse.ArgumentParser()
+from pre_utils import gen_system
+
 # user-input
-lx = 99.9       # system length x-dir
-ly = 100        # system length y-dir
-ax = 50         # initial x-coordinate of asperity
-ay = 50         # initial y-coordinate of asperity
-hl = 50         # height of lower surface/plate
-hu = 65         # height of upper surface
-hup = 2         # height of upper plate
-lz = hl + hu    #total system height
-octa_d = 1 * 39.0 #The multiplyer has to be an integer
-dode_d = 1 * 37.3
+lx = 100    # system length x-dir
+ly = 100      # system length y-dir
+ax = 50      # initial x-coordinate of asperity
+ay = 50      # initial y-coordinate of asperity
+hl = 50       # height of lower surface/plate
+hu = 50      # height of upper surface
+hup = 2       # height of upper plate
 
-grid = (4,4) 
+octa_d = 1 * 39.0
+dode_d = 1 * 37.3
 
 lower_orient = "100"
 remove_atoms = True
-erratic = True
 
-path = '../../initial_system/'
+#path = '../../initial_system/'
+path = ""
 
-
-if erratic and grid:
-    system = erratic_setup(lx, ly, ax, ay, hl, hu, hup, octa_d, dode_d, lower_orient,
-                           remove_atoms, path, grid=grid)
-
-    print(system)
-    system.set_cell(np.diag(np.max(system.positions, axis=0)))
-    system.wrap()
-
-    system_file = path + f"erratic/system_or{lower_orient}_hi{lz}_errgrid{grid[0]}_{grid[1]}.data"
-
-    write(system, system_file, atom_style = 'atomic')
-    #system.write(system_file, format="lammps-data", atom_style = 'atomic') #alternate write method that fixes error?
-    print("System written to: ", system_file)
-
-elif grid:
-    system = gen_grid_system(lx, ly, ax, ay, hl, hu, hup, octa_d, dode_d, lower_orient,
-                             remove_atoms, path, grid=grid)
-
-    system_file = path + f"grid/system_or{lower_orient}_hi{lz}_grid{grid[0]}_{grid[1]}.data"
-    system.write(system_file, format="lammps-data", atom_style = 'atomic')
-    print("System written to: ", system_file)
-
-else:
-    
-    system = gen_system(lx, ly, ax, ay, hl, hu, hup, octa_d, dode_d, lower_orient,
-                        remove_atoms, path)
-
-    system_file = path + f"system_or{lower_orient}_hi{lz}.data"
-    system.write(system_file, format="lammps-data", atom_style = 'atomic')
-    #write(system, system_file)
-    print("System written to: ", system_file)
-
-
-#print(system)
-#system.set_cell(np.diag(np.max(system.positions, axis=0)))
-#system.wrap()
-##system_file = path + f"system_or{lower_orient}_hi{lz}_grid{grid[0]}_{grid[1]}.data"
-#write(system, system_file)
-##system.write(system_file, format="lammps-data")
-##print("System written to: ", system_file)
-
-
+gen_system(lx, ly, ax, ay, hl, hu, hup, octa_d, dode_d, lower_orient,
+           remove_atoms, path)
