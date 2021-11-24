91e2745ce8d63a4cab97137f2bd3bd9be28be5f8
andebraa <andebraa@uio.no>
2021-11-24 18:05:31 +0100

    added manual_1asperity_slice which seems to actually cut out an sperity

--- python/post/ovitos_test_pipeline.py
+++ python/post/ovitos_test_pipeline.py
@@ -1,8 +1,15 @@
 from ovito.io import import_file, export_file
+from ovito.modifiers import *
+from ovito.pipeline import *
 
-pipeline = import_file(dumpfile, multiple_frames = True)
+dumpfile = '/home/andebraa/master/initial_system/erratic/system_or100_hi115_errgrid3_3.data'
 
-i = 1
+pipeline = import_file(dumpfile)#, multiple_frames = True)
+
+lx = 100.27080000000001
+ly = 100.27080000000001
+
+i = 0
 j = 0
 
 # first outward slice X direction
@@ -10,6 +17,7 @@ pipeline.modifiers.append(SliceModifier(
 distance = (i+1)*lx,
 normal = (1.0, 0.0, 0.0)))
 print('i+1 *lx=', (i+1)*lx)
+print('inverse false')
 
 # first inward slice X direction
 pipeline.modifiers.append(SliceModifier(
@@ -17,12 +25,14 @@ distance = (i)*lx,
 normal = (1.0, 0.0, 0.0),
 inverse = True))
 print('i *lx=', (i)*lx)
+print('inverse')
 
 # first outward slice Y direction
 pipeline.modifiers.append(SliceModifier(
 distance = (j+1)*ly,
 normal = (0.0, 1.0, 0.0)))
 print('j+1 *ly=', (j+1)*ly)
+print('inverse false')
 
 # first outward slice Y direction
 pipeline.modifiers.append(SliceModifier(
@@ -30,5 +40,6 @@ distance = (j)*ly,
 normal = (0.0, 1.0, 0.0),
 inverse = True))
 print('j *ly=', (j)*ly)
+print('inverse')
 
-pipeline.ass_to_scene()
+pipeline.add_to_scene()
