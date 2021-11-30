8d615548fa7cceb30fed17ed4cb7d2acffdf2ec5
andebraa <andebraa@uio.no>
2021-11-30 19:04:39 +0100

    merge conflict

--- python/post/erratic_relax_area.py
+++ python/post/erratic_relax_area.py
@@ -6,7 +6,7 @@ Largely based on Even's crystal aging project get_contact_area
 import json 
 import numpy as np
 import re
-from post_utils import get_erratic_contact_area
+#from post_utils import get_erratic_contact_area #ovitos doesn't like this, copied erratic_contact area in here instead
 from ovito.io import import_file, export_file
 from ovito.modifiers import *
 from ovito.pipeline import *
@@ -66,7 +66,7 @@ def erratic_relax_area():
         for dumpfile in dumpfiles:
             
             pipeline = import_file(dumpfile, multiple_frames = True)
-
+            pipeline.add_to_scene()
             for i in range(grid[0]):
                 for j in range(grid[1]):
                     if bool_grid[i,j]: #is boo_grid is 1, we have an asperity
@@ -96,7 +96,8 @@ def slicer_dicer(pipeline, i, j, lx, ly):
     distance = (i+1)*lx,
     normal = (1.0, 0.0, 0.0)))
     print('i+1 *lx=', (i+1)*lx)
-
+    
+    #pipeline.add_to_scene()
     # first inward slice X direction
     pipeline.modifiers.append(SliceModifier(
     distance = (i)*lx,
@@ -116,6 +117,80 @@ def slicer_dicer(pipeline, i, j, lx, ly):
     normal = (0.0, 1.0, 0.0),
     inverse = True))
     print('j *ly=', (j)*ly)
+
+
+def get_erratic_contact_area(pipeline, outfile="area.txt", delta=None,
+                             init_time=0, asperity = 1, grid = (1,1)):
+    """Get contact area and number of atoms in the
+    contact region as a function of time. Utilizing
+    Ovito. This script takes a cut out block containing an asperity
+    from stress_analysis.py.
+    writes a txt for each asperity, numbered from 0
+
+    Parameters
+    ----------
+    pipeline : ovito object?
+        pipeline that has cut out the single asperity
+    outfile : str
+        file to write the contact area to
+    delta : float
+        time difference between two frames in ns
+        (assume constant delta)
+    init_time : float
+        initial time given in ns
+    asperity : int
+        the number of the asperity
+    """
+
+    export_file(pipeline, 'test_block_asperity{}'.format(asperity), 'lammps/data', atom_style = 'atomic')
+    print('start of get-conctact_area')
+    if delta is None:
+        warnings.warn(r"No $\Delta t$ is given, setting $\Delta t=1$")
+        delta = 1
+
+    # Slice:
+    pipeline.modifiers.append(SliceModifier(
+        distance = 55,
+        normal = (0.0, 0.0, 1.0),
+        slab_width = 2.0))
+
+    # Cluster analysis:
+    pipeline.modifiers.append(ClusterAnalysisModifier(sort_by_size=True))
+    # Expression selection:
+    pipeline.modifiers.append(ExpressionSelectionModifier(expression='Cluster!=1'))
+
+    # Delete selected:
+    pipeline.modifiers.append(DeleteSelectedModifier())
+
+    # Construct surface mesh:
+    pipeline.modifiers.append(ConstructSurfaceModifier(radius=20.0, identify_regions=True))
+
+    # Output surface area as a function of time
+    times, nums, areas = [], [], []
+    for i in trange(pipeline.source.num_frames):
+        data = pipeline.compute(frame=i)
+        nums.append(data.attributes['ClusterAnalysis.largest_size'])
+        # divid area by 2 to find area of one surface and by 100 to go from Å² to nm²
+        areas.append(data.attributes['ConstructSurfaceMesh.surface_area'] / 200)
+
+        time = i * delta + init_time
+        times.append(time)
+    del pipeline
+
+    outfile_ = outfile +'_asperity'+str(asperity)+'.txt'
+
+    header = ("Crystal Aging Project \n"
+              "Author: Even Marius Nordhageni & Anders Bråte \n"
+              "\n"
+              "Contact area between asperity and lower surface and \n"
+              "number of particles in the contact region.  \n"
+              "\n"
+              "time [ns]\t\t # contact atoms\t contact area [nm^2]\n"
+              "number of this asperity: "+str(asperity))
+
+    savetxt(outfile_, asarray([times, nums, areas], dtype=float).T, header=header)
+    print('end of get contact area')
+    return times, nums, areas
 
 
 
