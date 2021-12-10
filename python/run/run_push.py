73a2c7d56a23073a2a9e297f335690645f848eff
andebraa <andebraa@uio.no>
2021-12-10 12:27:38 +0000

    boolean expressions are hard

--- python/run/run_push.py
+++ python/run/run_push.py
@@ -50,41 +50,51 @@ elif grid:
 else:
     relax_dir = project_dir + f"simulations/sys_or{orientation}_hi{height}/relax/"
     push_dir = project_dir + f"simulations/sys_or{orientation}_hi{height}/push/"
-print(os.listdir(relax_dir))
+#print(os.listdir(relax_dir))
 
 
 # push asperity
 relax_time = 1000 #ps time until we push
-time = 25000 # how long we push for (ps maybe, or timesteps)
-for seed in [94276]:
+relax_steps = 500000
+push_steps = 25000 # how long we push for (ps maybe, or timesteps)
+push_time = 1000
+
+for seed in [10730]:
     
     if grid:
-        restartfile = relax_dir + f"sim_temp{temp}_force{force}_time{relax_time}_seed{seed}_grid{grid[0]}_{grid[1]}/time.{relax_time}.restart"
+        if erratic:
 
-    elif erratic:
-        restartfile = relax_dir + f"sim_temp{temp}_force{force}_time{relax_time}_seed{seed}_errgrid{grid[0]}_{grid[1]}/time.{relax_time}.restart"
+            restartfile = relax_dir + f"sim_temp{temp}_force{force}_time{relax_time}_seed{seed}_errgrid{grid[0]}_{grid[1]}/time.{relax_steps}.restart"
+        else:
+            restartfile = relax_dir + f"sim_temp{temp}_force{force}_time{relax_time}_seed{seed}_grid{grid[0]}_{grid[1]}/time.{relax_steps}.restart"
+
 
     else:
-        restartfile = relax_dir + f"sim_temp{temp}_force{force}_time{relax_time}_seed{seed}/time.{relax_time}.restart"
+        restartfile = relax_dir + f"sim_temp{temp}_force{force}_time{relax_time}_seed{seed}/time.{relax_steps}.restart"
 
     var = {'restartfile': f"time.{relax_time}.restart",
            'paramfile': "SiC.vashishta",
            'temp': temp,
            'seed': seed,
            'force': 0.001,
-           'simtime': simtime,
-           'pushtime': relax_time, #time until push
+           'simtime': push_time, #even called it simetime, no idea what it is
+           'pushtime': relax_steps, #steps until push
            'vel': 5} #m/s
 
-    if erratic:
-        sim = Simulator(directory=push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{time}_seed{seed}_errgrid{grid[0]}_{grid[1]}", overwrite=True)
+    if grid: #system might not be erratic, but it could still be grid 
+        
+        if erratic:
+            print(push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{push_time}_seed{seed}_errgrid{grid[0]}_{grid[1]}")
+            
+            sim = Simulator(directory=push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{push_time}_seed{seed}_errgrid{grid[0]}_{grid[1]}", overwrite=True)
+
+        else:
+            sim = Simulator(directory=push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{push_time}_seed{seed}_grid{grid[0]}_{grid[1]}", overwrite=True)
     
-    elif grid: #system might not be erratic, but it could still be grid 
-        sim = Simulator(directory=push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{relax}_seed{seed}_grid{grid[0]}_{grid[1]}", overwrite=True)
 
     else:
-        sim = Simulator(directory=push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{relax}_seed{seed}", overwrite=True)
+        sim = Simulator(directory=push_dir + f"sim_temp{temp}_vel{vel}_force{force}_time{relax_steps}_seed{seed}", overwrite=True)
     
     sim.copy_to_wd(restartfile, lammps_dir + "SiC.vashishta", "sigmoid.py")
     sim.set_input_script(lammps_dir + "in.push", **var)
-    sim.run(computer=SlurmGPU(lmp_exec="lmp_python", slurm_args={'job-name': f'{int(temp/100)}_{time)}_{seed}'}, lmp_args={'-pk': 'kokkos newton on neigh full'}))
+    sim.run(computer=SlurmGPU(lmp_exec="lmp_python", slurm_args={'job-name': f'{int(temp/100)}_{relax_steps}_{seed}_erratic{erratic}'}, lmp_args={'-pk': 'kokkos newton on neigh full'}))
