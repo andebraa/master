3eb427d0a5d4ec7f6a4c5b15eea13278c860d83f
andebraa <andebraa@uio.no>
2022-03-16 16:21:35 +0000

    max static chess plot 1800

--- python/plot/plot_max_static.py
+++ python/plot/plot_max_static.py
@@ -29,6 +29,7 @@ def load_max_static(temp, vel, force, orientation, grid, template_lc, template_m
     for file in glob(ms_files):
         for seed in seeds:
             if str(seed) in str(file):
+                print(seed)
                 time, ms = loadtxt(file)
                 ms_all.append(ms)
 
@@ -38,8 +39,7 @@ def load_max_static(temp, vel, force, orientation, grid, template_lc, template_m
 
 def plot_max_static_vs_thiccness():
     # user input
-    temp = 2300
-    vel = [5,1.25]
+    temp = 1800
     force = 0.001
     orientation = 100
     grid = (4,4)
@@ -50,86 +50,46 @@ def plot_max_static_vs_thiccness():
     project_dir = '../../'
     fig_dir = project_dir + 'fig/'
 
-    # first run vel 5 time 500
-    _seeds_5 =  [88753, 12754, 91693] # thicc 80
-    _seeds_8 =  [50219, 31693, 19478] # thicc 83
-    _seeds_9 =  [81094, 81717, 10840] # thicc 85
-    _seeds_11 = [89936, 25987, 82860] # thicc 85
-    _seeds_12 = [63620, 46351, 18735] # thicc 90
-    _seeds_14 = [62648, 89682, 30607] # thicc 90
-    _seeds_16 = [17767, 58195, 19686] # thicc 95
-    _seeds_18 = [11084, 50879, 83355] # thicc 93
-    _seeds_20 = [81955, 86485, 61282] # thicc 95
-    _seeds_22 = [96804, 43682, 88372] # thicc 100
-    _seeds_24 = [64771, 82158, 63871] # thicc 100
-    _seeds_26 = [95317, 18462, 11729]#thicc 100
-    _seeds_28 = [54648,88413,94253] #thicc 103
-    _seeds_30 = [92651,19224,85380] #thicc 105
-    _seeds_33 = [64798,34149] #thicc 109
-    
-
-
-    # second push run vel 1.25
-    seeds_5 = [71361,91111,63445] # 80
-    seeds_8 = [12890,62608,29899] # 83
-    seeds_9 = [70529,45585,64900]
-    seeds_11 = [66909,27035,43111] # 85 
-    seeds_12 = [81809,93141,56900]
-    seeds_14 = [33447,88504,79793] # 90
-    seeds_18 = [75942,21663,49508] # 93
-    seeds_16 = [67321,29830,36667]
-    seeds_20 = [77113,99307,62931] # 95
-    seeds_22 = [47606,72506,44762]
-    seeds_24 = [47009,37801,56714]
-    seeds_26 = [54238,10257,73002] # 100
-    seeds_28 = [71970,74062,60542] # 103
-    seeds_30 = [81636,35286,69718] # 105
-    seeds_33 = [90953,90762,47980] # 109
-
-    seeds = {1.25: [seeds_5, seeds_8, seeds_9, seeds_11, seeds_12, seeds_14, 
-            seeds_16, seeds_18, seeds_20, seeds_22, seeds_24, seeds_26, 
-            seeds_28, seeds_30, seeds_33], 5: [_seeds_5, _seeds_8, _seeds_9, _seeds_11, _seeds_12, _seeds_14, 
-            _seeds_16, _seeds_18, _seeds_20, _seeds_22, _seeds_24, _seeds_26, 
-            _seeds_28, _seeds_30, _seeds_33]}
-
-
-    hup = [5,8,9,11,12,14,16,18,20,22,24,26,28,30,33]
 
     load_curve_dir = project_dir + 'txt/load_curves/erratic/'
     max_static_dir = project_dir + 'txt/max_static/erratic/'
 
-    template_lc = load_curve_dir + 'load_curves_temp{}_vel{}_force{}_or{}_seed*_errgrid{}_{}.txt'
-    template_ms = max_static_dir + 'max_static_temp{}_vel{}_force{}_or{}_seed*_errgrid{}_{}.txt'
+    template_lc = load_curve_dir + 'load_curves_temp{}_vel{}_force{}_or{}_seed*_errgrid{}_{}_chess.txt'
+    template_ms = max_static_dir + 'max_static_temp{}_vel{}_force{}_or{}_seed*_errgrid{}_{}_chess.txt'
     style.use('seaborn') 
     #fig, ax = plt.subplots(2,15)
     #axs = axs.ravel()
-    all_mean_static = np.zeros(len(seeds[1.25])*2)
 
-    fig_num = 0
-   
+    seed_dict_5 = {1: [36015,37461,77220], 2: [49156,64486,73803],
+                 3: [30642,74822,90272], 4: [20939,86781,87609],
+                 5: [25642,46012,71581], 6: [86406,91501]}
     colours = {1.25: 'go', 5:'ro'}
-    for vel, _seeds in seeds.items():
-        for i, seed in enumerate(_seeds):
+    
+    vel_seed_uc = {5: seed_dict_5} #, 1.25: seed_dict_125}
+    
+    all_mean_static = np.zeros(len(seed_dict_5)) #num keys
+    
+    for vel, seed_dict in vel_seed_uc.items():
+        for i, (uc, seed) in enumerate(seed_dict.items()):
             ms_all, mean_static = load_max_static(temp, vel, force, orientation, grid, 
                                                   template_lc, template_ms, seed) 
             all_mean_static[i] = mean_static
-            plt.plot(hup[i], mean_static, colours[vel])
+            plt.plot(uc, mean_static, colours[vel])
             #for ms in ms_all:
             #    plt.plot(hup[i], ms, 'ob')
     
-    plt.legend()
 
     plt.xlabel(r"$hup$ [pm]")
     plt.ylabel(r"$f$ [$\mu$N]")
     plt.title('mean max static vs upper plate thicness, green: 1.25, red: 5')
-    plt.savefig(fig_dir + 'png/max_static_vs_hup2.png')
+    plt.savefig(fig_dir + 'png/max_static_chess.png')
 
     
 
 if __name__ == '__main__':
 
     # user input
-    temp = 2300
+    temp = 1800
     vel = 5
     force = 0.001
     orientation = 100
