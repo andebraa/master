31dc9b7ee1eeedac45082d244d61452cf4a91758
andebraa <andebraa@uio.no>
2022-04-20 15:37:46 +0000

    max_z writes elements to file. need to sort that out

--- python/post/max_z.py
+++ python/post/max_z.py
@@ -1,18 +1,29 @@
 from ovito.io import *
 from ovito.modifiers import *
 import numpy
+from glob import glob
 
 def compute_max_z(frame, data):
     data.attributes["High_Z"] = numpy.max(data.particles["Position"][:,2])
 
-max_zs = []
-for force in [0, 0.001, 0.01, 0.1]:
-    dumpfiles = f'../../simulations/sys_asp{asperities}_uc{uc}/production/sim_temp{temp}_force{force}_asp{asperities}_time{time}_initnum{initnum}_seed*_errgrid4_4/dump.bin'
-    for dumpfile in glob(dumpfiles):
+def max_z_finder():
+    asperities = 8
+    uc = 5
+    temp = 2300
+    time = 1000
+    initnum = 0
 
-        pipeline = import_file(dumpfile)
+    max_zs = []
+    for force in [0, 0.001, 0.01, 0.1]:
+        dumpfiles = f'../../simulations/sys_asp{asperities}_uc{uc}/production/sim_temp{temp}_force{force}_asp{asperities}_time{time}_initnum{initnum}_seed*_errgrid4_4/dump.bin'
+        for dumpfile in glob(dumpfiles):
 
-        print(pipeline.data.particles['Position'][:,2])
-        stop
-        #pipeline.modifiers.append(compute_max_z)
-        #export_file(pipeline, "max_z.txt", "txt", columns=["Frame", "High_Z"], multiple_frames = True)
+            pipeline = import_file(dumpfile)
+
+            #print(pipeline.data.particles['Position'][:,2])
+            #stop
+            pipeline.modifiers.append(compute_max_z)
+            export_file(pipeline, "max_z.txt", "txt", columns=["Frame", "High_Z"], multiple_frames = True)
+            stop
+if __name__ == '__main__':
+    max_z_finder()
