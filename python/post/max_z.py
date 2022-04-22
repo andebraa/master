da499b1f80fa413983de8b64f0b7eb4142d3a0f4
andebraa <andebraa@uio.no>
2022-04-22 13:13:05 +0000

    plot of max height vs frame

--- python/post/max_z.py
+++ python/post/max_z.py
@@ -1,21 +1,19 @@
+import pandas as pd
 from ovito.io import *
 import re
 from ovito.modifiers import *
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 from glob import glob
 
 def compute_max_z(frame, data):
     data.attributes["High_Z"] = numpy.max(data.particles["Position"][:,2])
 
-def max_z_finder():
-    asperities = 8
-    uc = 5
-    temp = 2300
-    time = 1000
-    initnum = 0
+def max_z_finder(asperities, uc, temp, time, initnum):
 
     highz_dir = '../../txt/high_z/'
     max_zs = []
+    avg_max = []
     for force in [0, 0.001, 0.01, 0.1]:
         dumpfiles = f'../../simulations/sys_asp{asperities}_uc{uc}/production/sim_temp{temp}_force{force}_asp{asperities}_time{time}_initnum{initnum}_seed*_errgrid4_4/dump.bin'
         for dumpfile in glob(dumpfiles):
@@ -27,6 +25,43 @@ def max_z_finder():
             #stop
             pipeline.modifiers.append(compute_max_z)
             output = pipeline.compute()
-            export_file(pipeline, highz_dir + "maxz_temp{temperature}_force{force}_asp{asperities}_time{time}_initnum{initnum}_seed{seed}.txt", "txt", columns=["Frame", "High_Z"], multiple_frames = True)
+            outfiles = highz_dir + f"maxz_temp{temp}_force{force}_asp{asperities}_time{time}_initnum{initnum}_seed{seed}.txt"
+            export_file(pipeline, outfiles, "txt", columns=["Frame", "High_Z"], multiple_frames = True)
+    
+def plot_max_z(asperities, uc, temp, time, initnum):
+
+    for force in [0, 0.001, 0.01, 0.1]:
+        heights = []
+        files = highz_dir + f"maxz_temp{temp}_force{force}_asp{asperities}_time{time}_initnum{initnum}_seed*.txt"
+        for _file in glob(files):
+            seed = re.findall(r'\d', _file)[-1]
+            #df = pd.read_csv(_file, sep = '')
+            data = np.loadtxt(_file)
+            print('here')
+            print(data[0][:])
+            frames = []
+            height = []
+            for row in data:
+                frames.append(row[0])
+                height.append(row[1])
+            frames = np.array(frames)
+            height = np.array(height)
+            heights.append(height)
+
+            
+        avg_max.append(np.mean(heights, axis = 0))
+        print(frames, avg_max)
+        plt.plot(frames, avg_max[0], label = force)
+    plt.savefig('test.png')
 if __name__ == '__main__':
-    max_z_finder()
+    asperities = 8
+    uc = 5
+    temp = 2300
+    time = 1000
+    initnum = 0
+
+    highz_dir = '../../txt/high_z/'
+    max_zs = []
+    avg_max = []
+    #max_z_finder(asperities, uc, temp, time, initnum)
+    plot_max_z(asperities, uc, temp, time, initnum)
