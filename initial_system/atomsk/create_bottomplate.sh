55553d069f279b4011ebc2ffdc025bddbcea50b1
andebraa <andebraa@uio.no>
2022-05-30 11:01:20 +0200

    create_bottomplate seems to work now

--- initial_system/atomsk/create_bottomplate.sh
+++ initial_system/atomsk/create_bottomplate.sh
@@ -7,10 +7,20 @@
 #let "ly = $ly_tot/2"
 #let "lz = $lz_tot/2"
 
-atomsk --create diamond 4.3596 Si C -spacegroup 216 -orient [100] [010] [001] [10-1] [010] [101] -duplicate 200 200 20 sto_orig.xsf
-atomsk sto_orig.xsf -cut above 20 z sto_cut1.xsf
-atomsk sto_cut1.xsf -cut below -20 z sto_cut2.xsf
-atomsk sto_cut2.xsf -cut above 100 x sto_cut3.xsf
-atomsk sto_cut3.xsf -cut below -100 x sto_cut4.xsf
-atomsk sto_cut4.xsf -cut above 100 y sto_cut5.xsf
-atomsk sto_cut5.xsf -cut below -100 y sto_cut6.xsf
+atomsk --create diamond 4.3596 Si C -spacegroup 216 -orient [100] [010] [001] [10-1] [010] [101] -duplicate 100 0 100 testrun.xsf
+#100x100: origo in bottom corner. height 613, xmin xmax +/-305. midpoint z at 305 
+atomsk testrun.xsf -cut above 315 z xsf
+atomsk testrun.xsf -cut below 295 z xsf
+atomsk testrun.xsf -cut above 201 x xsf
+atomsk testrun.xsf -cut below -200 x xsf
+
+atomsk testrun.xsf -duplicate 0 92 0 xsf
+
+atomsk testrun.xsf -shift 200 0 0 testrun_shifted.xsf
+atomsk testrun_shifted.xsf -shift 0 0 -296
+
+
+# PRIMVEC
+#     401.0832000        0.00000000        0.00000000
+#      0.00000000      401.08320000        0.00000000
+#      0.00000000        0.00000000       18.47027233
