a24559454bba1efb6f3951fa30f3f68933564e0d
andebraa <andebraa@uio.no>
2022-06-13 06:57:34 +0000

    plot udpate

--- python/plot/plot_load_curves.py
+++ python/plot/plot_load_curves.py
@@ -440,7 +440,7 @@ def plot_production(temp, vel, force, uc, asperities, time, orientation, grid, e
             pass
             #axs[i].set_title(aux_dict['erratic'])
 
-        axs[i].set_ylim(bottom = -0.02, top = 0.05)
+        axs[i].set_ylim(bottom = -0.02, top = 0.06)
     plt.subplots_adjust(hspace=0.3)
     plt.suptitle(f"temp {temp}, force {force}, vel {vel}")
     plt.legend()
