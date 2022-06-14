0f5e0df60ce070e7b25f7336521c511f90261444
andebraa <andebraa@uio.no>
2022-06-14 07:44:30 +0000

    have to manually vary the y limits. kinda tedious

--- python/plot/plot_load_curves.py
+++ python/plot/plot_load_curves.py
@@ -445,7 +445,7 @@ def plot_production(temp, vel, force, uc, asperities, time, orientation, grid, e
             pass
             #axs[i].set_title(aux_dict['erratic'])
 
-        axs[i].set_ylim(bottom = -0.02, top = 0.06)
+        axs[i].set_ylim(bottom = -0.02, top = 0.1)
         axs[i].axvline(1.1, alpha = 0.7)
     plt.subplots_adjust(hspace=0.3)
     plt.suptitle(f"temp {temp}, force {force}, vel {vel}")
