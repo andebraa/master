2cd7fc4b82c6c9b9e2fd79acf507726efa79b965
andebraa <andebraa@uio.no>
2022-07-11 09:10:55 +0000

    ran till 75, also ml2 traiL

---
+++ python/ml/ml_trial2.py
@@ -0,0 +1,18 @@
+'''
+Attempt at 2d ncc using keras.
+https://towardsdatascience.com/mnist-cnn-python-c61a5bce7a19
+'''
+import keras
+from keras.datasets import fashion_mnist 
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data():
+    (train_x, train_y), (test_x, test_y) = fashion_mnist.load_data()
+    print(train_x)
+    print(train_y)
+
+load_data()
