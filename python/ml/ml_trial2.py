f05b6dcbd02e73a975c64920d4d7d443ed688a5e
andebraa <andebraa@uio.no>
2022-07-13 13:34:51 +0200

    make function for padding

--- python/ml/ml_trial2.py
+++ python/ml/ml_trial2.py
@@ -11,18 +11,135 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_data():
+def periodic_padding_flexible(tensor, axis,padding=1):
+    """
+        add periodic padding to a tensor for specified axis
+        tensor: input tensor
+        axis: on or multiple axis to pad along, int or tuple
+        padding: number of cells to pad, int or tuple
+
+        return: padded tensor
+        https://stackoverflow.com/questions/39088489/tensorflow-periodic-padding
+    """
+
+
+    if isinstance(axis,int):
+        axis = (axis,)
+    if isinstance(padding,int):
+        padding = (padding,)
+
+    ndim = len(tensor.shape)
+    for ax,p in zip(axis,padding):
+        # create a slice object that selects everything from all axes,
+        # except only 0:p for the specified for right, and -p: for left
+
+        ind_right = [slice(-p,None) if i == ax else slice(None) for i in range(ndim)]
+        ind_left = [slice(0, p) if i == ax else slice(None) for i in range(ndim)]
+        right = tensor[ind_right]
+        left = tensor[ind_left]
+        middle = tensor
+        tensor = tf.concat([right,middle,left], axis=ax)
+
+    return tensor
+
+def load_data(pp = 2):
     X = np.load('temp_out_matrix.npy')
     Y = np.load('temp_out_y.npy')
 
     xtrain, xtest, ytrain, ytest = train_test_split(X,Y, test_size=0.33, 
                                                     random_state = 69)
-    print(xtrain)
-    print('øøøøøøøøøøøøøøøøøøøøøøøøøø')
-    print(xtest)
-    print(ytrain)
-    print(ytest)
+    
+    #add padding
+    print(xtrain.shape)
+    xtrain_p = np.zeros((xtrain.shape[0], xtrain.shape[1]+pp, xtrain.shape[2]+pp))
+    print(xtrain_p.shape)
+
+
+    xtrain = xtrain.reshape(-1, 4,4, 1)
+    xtest = xtest.reshape(-1, 4,4, 1)
     return xtrain, xtest, ytrain, ytest
 
-load_data()
+xtrain, xtest, ytrain, ytest = load_data()
+
+print(xtrain.shape)
+print(xtest.shape)
+
+stop
+
+ytrain = to_categorical(ytrain)
+ytest = to_categorical(ytest)
+
+model = Sequential()
+
+model.add(Conv2D(64, (3,3), input_shape=(4, 4, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=5)
+'''
+import keras
+from keras.datasets import fashion_mnist 
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
+(train_X,train_Y), (test_X,test_Y) = fashion_mnist.load_data()
+
+train_X = train_X.reshape(-1, 28,28, 1)
+test_X = test_X.reshape(-1, 28,28, 1)
+
+train_X = train_X.astype('float32')
+test_X = test_X.astype('float32')
+train_X = train_X / 255
+test_X = test_X / 255
+
+train_Y_one_hot = to_categorical(train_Y)
+test_Y_one_hot = to_categorical(test_Y)
+
+model = Sequential()
+
+model.add(Conv2D(64, (3,3), input_shape=(28, 28, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
+print('Test loss', test_loss)
+print('Test accuracy', test_acc)
+
+predictions = model.predict(test_X)
+print(np.argmax(np.round(predictions[0])))
+
+plt.imshow(test_X[0].reshape(28, 28), cmap = plt.cm.binary)
+plt.show()
+
+'''
+
 
