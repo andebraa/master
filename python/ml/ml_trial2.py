cc7adb4f8e9a4374836c6e3b56cd5e3a39749884
andebraa <andebraa@uio.no>
2022-07-13 10:33:51 +0000

    moving to beefboy. nodes don't have keras

--- python/ml/ml_trial2.py
+++ python/ml/ml_trial2.py
@@ -7,9 +7,22 @@ from keras.datasets import fashion_mnist
 from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
 from keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
 def load_data():
-    (train_x, train_y), (test_x, test_y) = fashion_mnist.load_data()
+    X = np.load('temp_out_matrix.npy')
+    Y = np.load('temp_out_y.npy')
+
+    xtrain, xtest, ytrain, ytest = train_test_split(X,Y, test_size=0.33, 
+                                                    random_state = 69)
+    print(xtrain)
+    print('øøøøøøøøøøøøøøøøøøøøøøøøøø')
+    print(xtest)
+    print(ytrain)
+    print(ytest)
+    return xtrain, xtest, ytrain, ytest
+
 load_data()
+
