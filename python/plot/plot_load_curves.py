60dcc2d054e69b1af3485a16f16f604b40995fd6
andebraa <andebraa@uio.no>
2022-07-16 12:51:40 +0000

    more padding

--- python/plot/plot_load_curves.py
+++ python/plot/plot_load_curves.py
@@ -577,7 +577,7 @@ def load_vs_normal_force():
     
     plt.suptitle(f"temp {temp}, force {force}, vel {vel}")
     plt.legend()
-    fig.tight_layout(pad=0.5)
+    fig.tight_layout(pad=0.8)
     plt.savefig(fig_dir + 'varying_normalforce_chess.png')
 
 
