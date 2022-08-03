93244d02cb17d01cfa37d1e61f25be1e23d66617
andebraa <andebraa@uio.no>
2022-08-03 15:50:59 +0000

    CV results mst be only npz due to glob(*)

--- python/ml/plot_results.py
+++ python/ml/plot_results.py
@@ -19,13 +19,13 @@ for i,_file in enumerate(files):
     best_r2 = obj['arr_0'][0].r2_test
     if 'random' in _file:
         random.append((best_r2, best_mse))
-        axs[0].plot(i, best_r2, c='r')
-        axs[1].plot(i, best_mse, c='r')
+        axs[0].plot(i, best_r2, 'o',c='r')
+        axs[1].plot(i, best_mse, 'o',c='r')
     else:
         actual.append((best_r2, best_mse))
-        axs[0].plot(i, best_r2, c='b')
-        axs[1].plot(i, best_mse, c='b')
+        axs[0].plot(i, best_r2, 'o',c='b')
+        axs[1].plot(i, best_mse, 'o',c='b')
 
-fig.savefig('CV_results/ml_plot.png')
+fig.savefig('fig/ml_plot.png')
 print(random)
 print(actual)
