b0c3354c3b86b354531ce3ef69facf49ed883eb4
andebraa <andebraa@uio.no>
2022-08-03 16:04:22 +0000

    moved results into seperate folders

--- python/ml/plot_results.py
+++ python/ml/plot_results.py
@@ -3,29 +3,44 @@ from glob import glob
 from utils import *
 import matplotlib.pyplot as plt
 
-path = 'CV_results/'
+path_dnn = 'CV_results/dnn/'
+path_cnn = 'CV_results/cnn/'
 
-files = glob.glob(path+'*')
 
-random = []
-actual = []
+files_dnn = glob.glob(path_dnn+'*')
+files_cnn = glob.glob(path_cnn+'*')
+
+random_dnn = []
+actual_dnn = []
+
+random_cnn = []
+actual_cnn = []
 
 fig, axs = plt.subplots(2)
 axs = axs.ravel()
-print(files)
-for i,_file in enumerate(files):
+for i,_file in enumerate(files_dnn):
     obj = np.load(_file, allow_pickle = True)
     best_mse = obj['arr_0'][0].mse_test
     best_r2 = obj['arr_0'][0].r2_test
     if 'random' in _file:
-        random.append((best_r2, best_mse))
-        axs[0].plot(i, best_r2, 'o',c='r')
-        axs[1].plot(i, best_mse, 'o',c='r')
+        random_dnn.append((best_r2, best_mse))
+        axs[0].plot(best_mse, best_r2, 'o',c='r')
     else:
-        actual.append((best_r2, best_mse))
-        axs[0].plot(i, best_r2, 'o',c='b')
-        axs[1].plot(i, best_mse, 'o',c='b')
+        actual_dnn.append((best_r2, best_mse))
+        axs[0].plot(best_mse, best_r2, 'o',c='b')
 
+
+for i,_file in enumerate(files_cnn):
+    obj = np.load(_file, allow_pickle = True)
+    best_mse = obj['arr_0'][0].mse_test
+    best_r2 = obj['arr_0'][0].r2_test
+    if 'random' in _file:
+        random_cnn.append((best_r2, best_mse))
+        axs[1].plot(best_mse, best_r2, 'o',c='r')
+    else:
+        actual_cnn.append((best_r2, best_mse))
+        axs[1].plot(best_mse, best_r2, 'o',c='b')
+
+axs[1].set_title('cnn')
+axs[0].set_title('dnn')
 fig.savefig('fig/ml_plot.png')
-print(random)
-print(actual)
