3d97480841887209d9ec8100919ebedabb54ab0a
andebraa <andebraa@uio.no>
2022-08-17 10:02:51 +0000

    added sigmax results

--- python/ml/plot_results.py
+++ python/ml/plot_results.py
@@ -20,6 +20,9 @@ actual_cnn = []
 fig, axs = plt.subplots(2)
 axs = axs.ravel()
 c = plt.cm.viridis((1 - np.arange(2))/(1 - 0 + 0.1))
+
+sigmax = True
+
 for i,_file in enumerate(files_dnn):
     obj = np.load(_file, allow_pickle = True)
     best_mse = obj['arr_0'][0].mse_test
@@ -27,14 +30,23 @@ for i,_file in enumerate(files_dnn):
     print('i, best config')
     print(_file)
     print(i, obj['arr_0'][0])
-    if 'random' in _file:
-        random_dnn.append((best_r2, best_mse))
-        axs[0].plot(best_mse, best_r2, 'o',c=c[0])
-        print('^^random')
+    if not sigmax:
+        if 'random' in _file:
+            random_dnn.append((best_r2, best_mse))
+            axs[0].plot(best_mse, best_r2, 'o',c=c[0])
+            print('^^random')
+        else:
+            actual_dnn.append((best_r2, best_mse))
+            axs[0].plot(best_mse, best_r2, 'o',c=c[1])
     else:
-        actual_dnn.append((best_r2, best_mse))
-        axs[0].plot(best_mse, best_r2, 'o',c=c[1])
-
+        if 'sigmax' in _file:
+            if 'random' in _file:
+                random_dnn.append((best_r2, best_mse))
+                axs[0].plot(best_mse, best_r2, 'o',c=c[0])
+                print('^^random')
+            else:
+                actual_dnn.append((best_r2, best_mse))
+                axs[0].plot(best_mse, best_r2, 'o',c=c[1])
 
 for i,_file in enumerate(files_cnn):
     obj = np.load(_file, allow_pickle = True)
@@ -42,13 +54,28 @@ for i,_file in enumerate(files_cnn):
     best_r2 = obj['arr_0'][0].r2_test
     print('i, best config')
     print(i, obj['arr_0'][1])
-    if 'random' in _file:
-        random_cnn.append((best_r2, best_mse))
-        axs[1].plot(best_mse, best_r2, 'o',c=c[0])
-        print('^^random')
+    if not sigmax:
+        if 'random' in _file:
+            random_cnn.append((best_r2, best_mse))
+            axs[1].plot(best_mse, best_r2, 'o',c=c[0])
+            print('^^random')
+        else:
+            actual_cnn.append((best_r2, best_mse))
+            axs[1].plot(best_mse, best_r2, 'o',c=c[1])
     else:
-        actual_cnn.append((best_r2, best_mse))
-        axs[1].plot(best_mse, best_r2, 'o',c=c[1])
+        if 'sigmax' in _file:
+            if 'random' in _file:
+                random_cnn.append((best_r2, best_mse))
+                axs[1].plot(best_mse, best_r2, 'o',c=c[0])
+                print('^^random')
+            else:
+                actual_cnn.append((best_r2, best_mse))
+                axs[1].plot(best_mse, best_r2, 'o',c=c[1])
+
+if sigmax:
+    fig.suptitle('best gridsearch results using sigmax output')
+else:
+    fig.suptitle('best gridsearch results using max_static output')
 
 axs[1].set_title('cnn')
 axs[0].set_title('dnn')
@@ -57,4 +84,7 @@ axs[1].set_xlabel('mse')
 axs[0].set_ylabel('r2')
 axs[1].set_ylabel('r2')
 plt.tight_layout()
-fig.savefig('fig/ml_res.png', dpi = 200)
+if not sigmax:
+    fig.savefig('fig/ml_res.png', dpi = 200)
+else:
+    fig.savefig('fig/ml_res_sigmax.png', dpi = 200)
